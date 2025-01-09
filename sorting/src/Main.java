fc5f2b8a67f7dca275a83f26e50f97cc69435e2f
Nickel Yadav <nickelyadav57@gmail.com>
2025-01-09 11:33:23 +0530

    :tada: init: Completes project setup and scaffolding

---
+++ sorting/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {}
+
+    //TODO: Implement insertion sort
+    public static void insertionSort(ArrayList<Integer> unsortedList) {}
+
+    //TODO: Implement selectionSort
+    public static void selectionSort(ArrayList<Integer> unsortedList) {}
+
+    //TODO: Implement bubbleSort
+    public static void bubbleSort(ArrayList<Integer> unsortedList) {}
+}
