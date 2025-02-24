a4f71a1b937deeb276307db55efa25d58626d574
Nickel Yadav <nickelyadav57@gmail.com>
2025-02-24 11:24:55 +0530

    :sparkles: feat: Implements middle of linked list

--- twoPointers/src/Main.java
+++ twoPointers/src/Main.java
@@ -1,18 +1,43 @@
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 
+    public static class Node<T> {
+        public T val;
+        public Node<T> next;
+
+        public Node(T val) {
+            this(val, null);
+        }
+
+        public Node(T val, Node<T> next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public static int removeDuplicates(List<Integer> arr) {
         int slow = 0;
         for (int fast =0; fast < arr.size(); fast++) {
-            if (arr.get(fast) != arr.get(slow)) {
+            if (!Objects.equals(arr.get(fast), arr.get(slow))) {
                 slow++;
                 arr.set(slow, arr.get(fast));
             }
         }
         return slow + 1;
+    }
+
+    public static int middleOfLinkedList(Node<Integer> head) {
+        Node<Integer> slow = head;
+        Node<Integer> fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow.val;
     }
 }
